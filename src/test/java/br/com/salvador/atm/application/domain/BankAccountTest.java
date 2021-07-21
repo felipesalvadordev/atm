00284bf675372d0fd58701803aae006289336b45
Felipe Salvador <f.salvador91@gmail.com>
2021-07-21 18:57:06 -0300

    setup method BankAccountTest

--- src/test/java/br/com/salvador/atm/application/domain/BankAccountTest.java
+++ src/test/java/br/com/salvador/atm/application/domain/BankAccountTest.java
@@ -1,20 +1,27 @@
 package br.com.salvador.atm.application.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import java.math.BigInteger;
 
 class BankAccountTest {
+
+    private BankAccount account;
+
+    @BeforeEach
+    void setup(){
+        account = new BankAccount(new BigInteger("500"));
+    }
+
     @Test
     void testGetBalance(){
-        BankAccount account = new BankAccount(new BigInteger("500"));
         BigInteger balance = account.getBalance();
         assertThat(balance).isEqualTo(new BigInteger("500"));
     }
 
    @Test
     void testDeposit(){
-        BankAccount account = new BankAccount(new BigInteger("500"));
         account.deposit(new BigInteger("100"));
         BigInteger balance = account.getBalance();
         assertThat(balance).isEqualTo(new BigInteger("600"));
@@ -22,7 +29,6 @@ class BankAccountTest {
 
     @Test
     void testWithdraw(){
-        BankAccount account = new BankAccount(new BigInteger("500"));
         account.withdraw(new BigInteger("100"));
         BigInteger balance = account.getBalance();
         assertThat(balance).isEqualTo(new BigInteger("400"));
